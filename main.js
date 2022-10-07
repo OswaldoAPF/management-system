e0c10f2ea4d16c6bf4610742514b57e23298eb7c
BenTrip98 <ben.tripaylan@gmail.com>
2022-10-07 01:10:31 -0300

    arrays terminadas, pendiente: imprimir carrito en pagina

--- main.js
+++ main.js
@@ -1,32 +1,11 @@
-<<<<<<< HEAD
-const {createApp}= Vue
-
-createApp ({
-    data(){
-
-    },
-    created(){
-
-    },
-    mounted(){
-
-    },
-    methods:{
-
-    },
-    computed:{
-
-    }
-})
-=======
-
-
 const { createApp } = Vue
 
 createApp({
   data() {
     return {
-      shoes: []
+      shoes: [],
+      cart:[],
+      totalCart:[]
     }
   },
 
@@ -38,7 +17,29 @@ createApp({
   },
 
   methods:{
+    addToCart(shoe){
+      let boolean = this.cart.some( e => e.id === shoe.id)
+      console.log(boolean)
 
-  }
-}).mount('#app')
->>>>>>> 15abb2035577aa31e264f6809e4eb1e9dc38bc1f
+      if(boolean){
+        this.cart.forEach( e => {
+            if(e.id === shoe.id ){
+             e.inCart++
+             e.stock--
+             e.total += e.price
+            }
+        })
+    }else {this.cart.push(shoe)
+      shoe.inCart++
+      shoe.stock--
+      shoe.total=shoe.price
+      this.cart.inCart++
+      this.cart.stock--
+      this.cart.total+=this.cart.price
+    }
+
+      console.log(this.cart)
+      console.log(this.shoes)
+  },
+
+}}).mount('#app')
